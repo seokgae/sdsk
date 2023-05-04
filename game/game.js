2ac7739aa1fefd967c8eae7e9fbf5ba870c0171e
seokgae <132544088+seokgae@users.noreply.github.com>
2023-05-04 17:04:23 +0900

    Add files via upload

--- game/game.js
+++ game/game.js
@@ -6,17 +6,21 @@ var player = 0;
 
 var com = Math.floor(Math.random()*3)+1;
 
+while (true) {
+    var input = prompt("가위 바위 보");
 
-var input = prompt("가위 바위 보");
-
-if (input == "가위")
-    {player = 1;}
-else if (input == "바위")
-    {player = 2;}
-else if (input == "보")
-    {player = 3;}
+    if (input == "가위")
+        {player = 1;break;}
+    else if (input == "바위")
+        {player = 2;break;}
+    else if (input == "보")
+        {player = 3;break;}
+    else
+        alert("다시하세요");
+}
 
 result = player - com;
+
 
 if((result == -2)||(result == 1))
     alert("win!");
