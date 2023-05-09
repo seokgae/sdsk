e7d6fd3e70624873c6332a020039e45d2725bd35
seokgae <132544088+seokgae@users.noreply.github.com>
2023-05-09 10:56:30 +0900

    Add files via upload

---
+++ rpg_v0/rpg_v1/common.js
@@ -0,0 +1,15 @@
+function br(){
+    document.write("<br>");
+}
+
+function hr(){
+    document.write("<hr>");
+}
+
+function RandomizeAttack(value) {
+    return Math.floor(Math.random()*value+1);
+}
+
+function Battle_start() {
+    hr();document.write("전투시작");hr();
+}
